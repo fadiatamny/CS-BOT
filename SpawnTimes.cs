71b57b5df3aa1e999e7f849503a76805916625b9
Fadi Atamny <fadiatamny@gmail.com>
2019-04-11 19:03:10 +0300

    Commenting

--- SpawnTimes.cs
+++ SpawnTimes.cs
@@ -10,6 +10,7 @@ namespace OurTinyBot
 
         public SpawnTimes()
         {
+            //init spawntimes set by game for bosses
             times = new List<TimeSpan>
             {
                 new TimeSpan(01, 15, 00),
@@ -22,7 +23,7 @@ namespace OurTinyBot
                 new TimeSpan(23, 15, 00)
             };
         }
-
+        //checks if current time t equals to any given boss spawntime 
         public int GetIndexTime(TimeSpan t)
         {
             for (int i = 0; i < times.Count; i++)
